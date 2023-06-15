37726ac8785edc4956cbaac093e16fd2c7e3e969
Mariia <maria.gadova@gmail.com>
2023-06-16 00:10:18 +0300

    Sem5_HW2

--- Sem5_HW/HW2.py
+++ Sem5_HW/HW2.py
@@ -3,3 +3,13 @@
 # *Пример:*
 # 2 2
 #     4 
+
+num1 = int(input("Введите первое число : "))
+num2 = int(input("Введите второе число : "))
+
+def plus (num1, num2):
+    if num2 == 0:
+        return num1
+    return plus(num1 + 1, num2 - 1)
+
+print(plus(num1, num2))
