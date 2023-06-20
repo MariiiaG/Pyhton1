7d069348e3ed75fcf24495d9ea8e88815342d342
Mariia <maria.gadova@gmail.com>
2023-06-20 12:33:56 +0300

    upd sem7

--- Sem7.py
+++ Sem7.py
@@ -39,17 +39,19 @@
 # а затем найти и сам эллипс, имеющий такую площадь. 
 #  Гарантируется, что самая далекая планета ровно одна
 
-import random
+# import random
 
-print(type_list := [(random.randint(1, 4), random.randint(1, 4)) for _ in range(10)])
+# print(type_list := [(random.randint(1, 4), random.randint(1, 4)) for _ in range(10)])
 
-type_list = list(filter(lambda x : x[0] != x[1], type_list))
-print(type_list := list(set(type_list)))
+# type_list = list(filter(lambda x : x[0] != x[1], type_list))
+# print(type_list := list(set(type_list)))
 
-new_list = list(map(lambda x : 3.14 * x[0] * x[1], type_list))
-print(new_list)
+# new_list = list(map(lambda x : 3.14 * x[0] * x[1], type_list))
+# print(new_list)
 
-for i in type_list:
-    if i[0] * i[1] * 3.14 == max(new_list):
-        print(i, max(new_list))
-        break
+# for i in type_list:
+#     if i[0] * i[1] * 3.14 == max(new_list):
+#         print(i, max(new_list))
+#         break
+
+print(test)
